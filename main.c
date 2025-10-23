8358642e01cdf805716a44bba83e14accab2c3e6
arthurMalveste <arthurmielemalveste@gmail.com>
2025-10-23 09:08:26 -0300

    criacao dos arquivos

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+
+// Venda ingressos 
+
+// Relatórios
+
+// Persistência dos dados
+
+// A função main vai ficar somente o menu e chamando as outras funções e arquivos
 int main(){
     printf("ola");
     return 0;
